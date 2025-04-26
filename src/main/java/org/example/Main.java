1f6dd29d5085b67cbc1e9d66350ce8935df2e34e
Fulloa08 <f.ulloa08@ufromail.cl>
2025-04-26 19:14:12 -0400

    desarrollo metodo FigurasGeometricas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -53,6 +55,46 @@ public class Main {
         double  m = (y2-y1) / (x2-x1);
         double b = y1 - (m*x1);
         return "Y = " + m + "X + " + b;
+    }
+
+}
+
+public class FigurasGeometricas {
+    public static double perimetroCuadrado(double lado){
+        return 4 * lado;
+    }
+    public static double areaCuadrado(double lado){
+        return lado * lado;
+    }
+    public static double perimetroRectangular(double base, double altura){
+        return 2 * (base + altura);
+    }
+    public static double areaRectangulo(double base, double altura){
+        return base * altura;
+    }
+    public static double perimetroCirculo (double radio ){
+        return 2 * Math.PI * radio;
+    }
+    public static double volumenEsfera(double radio){
+        return (4.0 / 3.0) * Math.PI * Math.pow(radio, 3);
+    }
+    public static double volumenCubo(double lado){
+        return Math.pow(lado, 3);
+    }
+    public static double areaCubo(double lado) {
+        return 6 * Math.pow(lado, 2);
+    }
+    public static double volumenCono(double radio, double altura) {
+        return (Math.PI * Math.pow(radio, 2) * altura) / 3;
+    }
+    public static double areaLateralCono(double radio, double generatriz){
+        return Math.PI * radio * generatriz;
+    }
+    public static void calcularFigurasGeometrica(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecciona una figura geométrica:");
+        System.out.println("");
     }
 
 }
